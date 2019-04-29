b6254e005f7d657a2a09088008c57e89cd439aaa
Mingyyy <j.yanming@gmail.com>
2019-04-29 15:19:43 +0800

    Complete Visa Info page V2

--- appapi/views.py
+++ appapi/views.py
@@ -12,8 +12,10 @@ def visa_view(request):
     cs = "US"
     d = "VN"
     requirement, portrestriction, allowedstay, type = "Unknown", "Unknown", "Unknown", "Unknown"
+    passport_validity, password_blank_pages = "Unknown", "Unknown"
     country_name, country_call_code, country_capital, country_timezone, country_population = \
         "Unknown", "Unknown", "Unknown","Unknown","Unknown"
+    ccy_arrival, ccy_exit = "Unknown","Unknown"
     textual=[]
     if request.GET:
         cs = request.GET["citizenship"]
@@ -21,31 +23,48 @@ def visa_view(request):
         messages.success(request, f"You are from {cs} and going to visit {d}.")
     else:
         form=EntryRequirementForm()
-    url = "https://requirements-api.sandbox.joinsherpa.com/v2/entry-requirements"
-    querystring = {"citizenship": cs, "destination": d, "language": lan,
-                   "key": secret.SHERPA_API}
-    headers = {'accept': '*/*'}
-    try:
-        response = requests.request("GET", url, headers=headers, params=querystring)
-        visa_info=response.json()
-        print(visa_info)
-        for k, v in visa_info.items():
-            if k == "visa":
-                for key, value in v[0].items():
-                    print(key)
-                    if key == "requirement":
-                        requirement = value
-                    elif key == "allowedStay":
-                        allowedstay= value
-                    elif key == "portRestriction":
-                        portrestriction = value
-                    elif key == "type":
-                        type = value
-                    elif key == "textual":
-                        for x in value["text"]:
-                            textual.append(x)
-    except:
-        messages.warning(request, "Sorry, visa requirement to this country is not available at the moment.")
+    if cs == d:
+        requirement = "NOT_REQUIRED"
+        portrestriction = "None"
+        allowedstay = "Unlimited"
+        type = "Citizenship"
+        textual = ["Probably you don't need a visa to visit your country."]
+    else:
+        url = "https://requirements-api.sandbox.joinsherpa.com/v2/entry-requirements"
+        querystring = {"citizenship": cs, "destination": d, "language": lan,
+                       "key": secret.SHERPA_API}
+        headers = {'accept': '*/*'}
+        try:
+            response = requests.request("GET", url, headers=headers, params=querystring)
+            visa_info=response.json()
+            print(visa_info)
+            for k, v in visa_info.items():
+                if k == "visa":
+                    for key, value in v[0].items():
+                        print(key)
+                        if key == "requirement":
+                            requirement = value
+                        elif key == "allowedStay":
+                            allowedstay= value
+                        elif key == "portRestriction":
+                            portrestriction = value
+                        elif key == "type":
+                            type = value
+                        elif key == "textual":
+                            for x in value["text"]:
+                                textual.append(x)
+                if k == "passport":
+                    passport_validity = v["passport_validity"]
+                    password_blank_pages = v["blank_pages"]
+
+                if k == "currency":
+                    ccy_exit = v["exit"]
+                    ccy_arrival = v["arrival"]
+
+                # if k == "vaccination":
+                #     vaccination_risk = v[]
+        except:
+            messages.warning(request, "Sorry, visa requirement to this country is not available at the moment.")
 
     try:
         country_info = requests.get(f"https://restcountries.eu/rest/v2/alpha/{d}").json()
@@ -53,22 +72,24 @@ def visa_view(request):
             if k == "name":
                 country_name = v
             elif k == "callingCodes":
-                country_call_code = v
+                country_call_code = v[0]
             elif k == "capital":
                 country_capital = v
             elif k == "population":
                 country_population = v
             elif k == "timezones":
-                country_timezone = v
+                country_timezone = v[0]
 
     except:
         messages.warning(request, "Sorry, there is no info for this country.")
 
     context = {"form": form, "requirement": requirement, "allowedstay": allowedstay,
-               "portrestriction":portrestriction,"type": type, "textual": textual,
+               "portrestriction": portrestriction,"type": type, "textual": textual,
                "country_name": country_name, "country_call_code": country_call_code,
-               "country_capital":country_capital, "country_population":country_population,
-               "country_timezone": country_timezone}
+               "country_capital": country_capital, "country_population": country_population,
+               "country_timezone": country_timezone, "passport_validity": passport_validity,
+               "passport_blank_pages": password_blank_pages,"currency_exit": ccy_exit,
+               "currency_arrival": ccy_arrival}
     return render(request, "appapi/visa.html", context)
 
 
